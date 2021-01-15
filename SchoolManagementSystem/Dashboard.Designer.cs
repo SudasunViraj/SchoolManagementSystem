2730f93e50cb8790d6ed0114d06c8c5a836b89c4
Indrajith <68956752+uldindrajith@users.noreply.github.com>
2021-01-15 05:53:21 +0530

    Add project files.

---
+++ SchoolManagementSystem/Dashboard.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace SchoolManagementSystem
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newAdmissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentAdmissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teacherAdmissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subjectAdmissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.examAdmissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchStudentToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feedbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutSchoolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.searchTeacherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchModuleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchExamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newAdmissionToolStripMenuItem,
+            this.searchStudentToolStripMenuItem,
+            this.deleteStudentToolStripMenuItem,
+            this.feedbackToolStripMenuItem,
+            this.aboutSchoolToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menuStrip1.Size = new System.Drawing.Size(816, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newAdmissionToolStripMenuItem
+            // 
+            this.newAdmissionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentAdmissionToolStripMenuItem,
+            this.teacherAdmissionToolStripMenuItem,
+            this.subjectAdmissionToolStripMenuItem,
+            this.examAdmissionToolStripMenuItem});
+            this.newAdmissionToolStripMenuItem.Name = "newAdmissionToolStripMenuItem";
+            this.newAdmissionToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.newAdmissionToolStripMenuItem.Text = "Admission";
+            // 
+            // studentAdmissionToolStripMenuItem
+            // 
+            this.studentAdmissionToolStripMenuItem.Name = "studentAdmissionToolStripMenuItem";
+            this.studentAdmissionToolStripMenuItem.Size = new System.Drawing.Size(253, 26);
+            this.studentAdmissionToolStripMenuItem.Text = "Student Admission";
+            this.studentAdmissionToolStripMenuItem.Click += new System.EventHandler(this.studentAdmissionToolStripMenuItem_Click);
+            // 
+            // teacherAdmissionToolStripMenuItem
+            // 
+            this.teacherAdmissionToolStripMenuItem.Name = "teacherAdmissionToolStripMenuItem";
+            this.teacherAdmissionToolStripMenuItem.Size = new System.Drawing.Size(253, 26);
+            this.teacherAdmissionToolStripMenuItem.Text = "Teacher Admission";
+            this.teacherAdmissionToolStripMenuItem.Click += new System.EventHandler(this.teacherAdmissionToolStripMenuItem_Click);
+            // 
+            // subjectAdmissionToolStripMenuItem
+            // 
+            this.subjectAdmissionToolStripMenuItem.Name = "subjectAdmissionToolStripMenuItem";
+            this.subjectAdmissionToolStripMenuItem.Size = new System.Drawing.Size(253, 26);
+            this.subjectAdmissionToolStripMenuItem.Text = "Subject Admission";
+            this.subjectAdmissionToolStripMenuItem.Click += new System.EventHandler(this.subjectAdmissionToolStripMenuItem_Click);
+            // 
+            // examAdmissionToolStripMenuItem
+            // 
+            this.examAdmissionToolStripMenuItem.Name = "examAdmissionToolStripMenuItem";
+            this.examAdmissionToolStripMenuItem.Size = new System.Drawing.Size(253, 26);
+            this.examAdmissionToolStripMenuItem.Text = "Exam Admission";
+            this.examAdmissionToolStripMenuItem.Click += new System.EventHandler(this.examAdmissionToolStripMenuItem_Click);
+            // 
+            // searchStudentToolStripMenuItem
+            // 
+            this.searchStudentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchStudentToolStripMenuItem1,
+            this.searchTeacherToolStripMenuItem,
+            this.searchModuleToolStripMenuItem,
+            this.searchExamToolStripMenuItem});
+            this.searchStudentToolStripMenuItem.Name = "searchStudentToolStripMenuItem";
+            this.searchStudentToolStripMenuItem.Size = new System.Drawing.Size(125, 24);
+            this.searchStudentToolStripMenuItem.Text = "Find Details";
+            // 
+            // searchStudentToolStripMenuItem1
+            // 
+            this.searchStudentToolStripMenuItem1.Name = "searchStudentToolStripMenuItem1";
+            this.searchStudentToolStripMenuItem1.Size = new System.Drawing.Size(225, 26);
+            this.searchStudentToolStripMenuItem1.Text = "Search Student";
+            this.searchStudentToolStripMenuItem1.Click += new System.EventHandler(this.searchStudentToolStripMenuItem1_Click);
+            // 
+            // deleteStudentToolStripMenuItem
+            // 
+            this.deleteStudentToolStripMenuItem.Name = "deleteStudentToolStripMenuItem";
+            this.deleteStudentToolStripMenuItem.Size = new System.Drawing.Size(178, 24);
+            this.deleteStudentToolStripMenuItem.Text = "Delete Information";
+            this.deleteStudentToolStripMenuItem.Click += new System.EventHandler(this.deleteStudentToolStripMenuItem_Click);
+            // 
+            // feedbackToolStripMenuItem
+            // 
+            this.feedbackToolStripMenuItem.Name = "feedbackToolStripMenuItem";
+            this.feedbackToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.feedbackToolStripMenuItem.Text = "School Information";
+            // 
+            // aboutSchoolToolStripMenuItem
+            // 
+            this.aboutSchoolToolStripMenuItem.Name = "aboutSchoolToolStripMenuItem";
+            this.aboutSchoolToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.aboutSchoolToolStripMenuItem.Text = "Exit System";
+            this.aboutSchoolToolStripMenuItem.Click += new System.EventHandler(this.aboutSchoolToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(14, 24);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(100, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(613, 384);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // searchTeacherToolStripMenuItem
+            // 
+            this.searchTeacherToolStripMenuItem.Name = "searchTeacherToolStripMenuItem";
+            this.searchTeacherToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.searchTeacherToolStripMenuItem.Text = "Search Teacher";
+            this.searchTeacherToolStripMenuItem.Click += new System.EventHandler(this.searchTeacherToolStripMenuItem_Click);
+            // 
+            // searchModuleToolStripMenuItem
+            // 
+            this.searchModuleToolStripMenuItem.Name = "searchModuleToolStripMenuItem";
+            this.searchModuleToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.searchModuleToolStripMenuItem.Text = "Search Module";
+            this.searchModuleToolStripMenuItem.Click += new System.EventHandler(this.searchModuleToolStripMenuItem_Click);
+            // 
+            // searchExamToolStripMenuItem
+            // 
+            this.searchExamToolStripMenuItem.Name = "searchExamToolStripMenuItem";
+            this.searchExamToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.searchExamToolStripMenuItem.Text = "Search Exam";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 438);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newAdmissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchStudentToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deleteStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feedbackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutSchoolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentAdmissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teacherAdmissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subjectAdmissionToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem examAdmissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchTeacherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchModuleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchExamToolStripMenuItem;
+    }
+}
---
+++ SchoolManagementSystem/SubjectAssign.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace SchoolManagementSystem
+{
+    partial class SubjectAssign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SubjectAssign));
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 238);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 24);
+            this.label4.TabIndex = 100;
+            this.label4.Text = "Teacher Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(201, 185);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(231, 29);
+            this.textBox3.TabIndex = 99;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(496, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 98;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Location = new System.Drawing.Point(231, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(186, 29);
+            this.label5.TabIndex = 97;
+            this.label5.Text = "Subject Assign";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(306, 289);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 39);
+            this.button2.TabIndex = 96;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(91, 289);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 39);
+            this.button1.TabIndex = 95;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.TabIndex = 94;
+            this.label3.Text = "Teacher ID";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(201, 127);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 29);
+            this.textBox2.TabIndex = 93;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 24);
+            this.label2.TabIndex = 92;
+            this.label2.Text = "Subject Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(201, 76);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 29);
+            this.textBox1.TabIndex = 91;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 90;
+            this.label1.Text = "Subject ID";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(201, 238);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(231, 29);
+            this.textBox4.TabIndex = 101;
+            // 
+            // SubjectAssign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 360);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "SubjectAssign";
+            this.Text = "ModuleAssign";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
